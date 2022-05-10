e7dccd6a4d52e4232f9f71d1c6c2532953a08c65
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-10 19:56:51 +0530

    Footer Page added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Developer from "./Components/developer/Developer";
 import Hero from "./Components/hero/Hero";
 import Navbar from "./Components/navbar/Navbar";
 import Subscribe from "./Components/subscribe/Subscribe";
-
+import Footer from "../src/Components/footer/Footer";
 function App() {
   return (
     <div>
@@ -14,6 +14,7 @@ function App() {
       <About />
       <Developer />
       <Subscribe />
+      <Footer />
     </div>
   );
 }
