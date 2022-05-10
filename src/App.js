8e2367955e8e62e63c4c03a43e639c819366e8d0
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-10 17:17:48 +0530

    Home page Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Hero from "./Components/hero/Hero";
 import Navbar from "./Components/navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
